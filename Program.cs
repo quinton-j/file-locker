8bbf30ca359683533cfec7142241360301f54502
Q <justin.quinton@mitel.com>
2018-02-27 20:49:33 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace FileLocker
+{
+    class Program
+    {
+        static void Main(string[] files)
+        {
+            Console.WriteLine($"Locking files:  {String.Join(",", files)}");
+
+            foreach (var filePath in files)
+            {
+                try
+                {
+                    var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None);
+                    Console.WriteLine($"Locked file {fileStream.Name} with handle {fileStream.SafeFileHandle.DangerousGetHandle()}");
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"Unable to lock file {filePath}:  {exception}");
+                }
+            }
+
+            Console.WriteLine("Press enter to terminate.");
+            Console.ReadLine();
+        }
+    }
+}
